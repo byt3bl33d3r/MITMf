e7a7b0a57fdd2e832d6bbe29c5a20c75399098be
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-03-10 21:55:44 +0100

    removed os specific deps in setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-apt-get install python-scapy python-dns python-pip msgpack-python python-nfqueue python-imaging -y
-apt-get install python-twisted-web python-dnspython python-requests python-configobj python-pefile -y
-pip install pyyaml ua-parser user-agents
 git submodule init
 git submodule update
 cd libs/bdfactory/ && ./install.sh
