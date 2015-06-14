f99080fc4cb3821b40676cdd4aea8f263051c05f
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-06-15 01:04:47 +0200

    fixed error in Exception handling in SMBserver.py

--- core/servers/smb/SMBserver.py
+++ core/servers/smb/SMBserver.py
@@ -62,7 +62,7 @@ class SMBserver(ConfigWatcher):
         
         except socketerror as e:
             if "Address already in use" in e:
-                shutdown("\n[-] Unable to start SMB server on port {}: port already in use".format(listenPort))
+                shutdown("\n[-] Unable to start SMB server on port {}: port already in use".format(self.smb_port))
 
     def configureLogging(self, formatter):
         #yes I know this looks awful, yuck
