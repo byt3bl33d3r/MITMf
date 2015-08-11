56cb34568d00080e900cb109ed041077a2e747e6
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-08-11 16:25:00 +0200

    added caspstone to build deps, added msg param in basic_tests.py

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -5,7 +5,7 @@ class BasicTests(unittest.TestCase):
 
 	def test_exec(self):
 		status, res = getstatusoutput('sudo python mitmf.py --help')
-		self.assertEqual(0, status)
+		self.assertEqual(0, status, msg=res)
 
 if __name__ == '__main__':
     unittest.main()
