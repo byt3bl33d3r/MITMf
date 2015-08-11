a44cf5cd29e969a5bb1e9753b47300fc0cbbd5ff
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-08-11 16:10:12 +0200

    doing some testing with tests

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -4,7 +4,7 @@ from commands import getstatusoutput
 class BasicTests(unittest.TestCase):
 
 	def test_exec(self):
-		status, res = getstatusoutput('python mitmf.py --help')
+		status, res = getstatusoutput('sudo python mitmf.py --help')
 		self.assertEqual(0, status)
 
 if __name__ == '__main__':
