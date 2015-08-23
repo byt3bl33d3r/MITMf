db5b65c463ae6ad68e4be73ab1e5e3a12cec4a38
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-08-23 20:11:27 +0200

    trying some new tests out

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,11 +1,23 @@
 import unittest
-from commands import getstatusoutput
+import logging
 
 class BasicTests(unittest.TestCase):
 
-	def test_exec(self):
-		status, res = getstatusoutput('sudo python mitmf.py --help')
-		self.assertEqual(0, status, msg=res)
+    def test_configfile(self):
+        from configobj import ConfigObj
+        config = ConfigObj('config/mitmf.conf')
+
+    def test_logger(self):
+        from core.logger import logger
+        logger.log_level = logging.DEBUG
+        formatter = logging.Formatter("%(asctime)s [unittest] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        log = logger().setup_logger("unittest", formatter)
+
+    def test_DNSChef(self):
+        from core.logger import logger
+        logger.log_level = logging.DEBUG
+        from core.servers.DNS import DNSChef
+        DNSChef().start()
 
 if __name__ == '__main__':
     unittest.main()
