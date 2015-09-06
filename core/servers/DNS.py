a0fecd4a381443f98b2d49de3d0eab9188fd9f5e
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-09-06 10:51:40 +0200

    reverts changes from PR #183, fixes issue #187

--- core/servers/DNS.py
+++ core/servers/DNS.py
@@ -449,7 +449,7 @@ class DNSChef(ConfigWatcher):
 
        # Use alternative DNS servers
         if config['nameservers']:
-            self.nameservers = config['nameservers']
+            self.nameservers = config['nameservers'].split(',')
 
         for section in config.sections:
 
